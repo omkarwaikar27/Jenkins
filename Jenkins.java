b332e995bc63fd45eb776d745dfec22320373b6a
omkarwaikar27 <60524038+omkarwaikar27@users.noreply.github.com>
2022-03-03 17:41:11 +0530

    Update Jenkins.java

--- Jenkins.java
+++ Jenkins.java
@@ -1,5 +1,6 @@
 public class Jenkins{
 	public static void main(String[] args) {
-        System.out.println("Hello Jenkins from Github"); 
+        System.out.println("Hello Jenkins from Github");
+		 System.out.println("Hello");
     }
 }
